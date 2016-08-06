ed01589c7593dd45481a406be0f16581263efa58
Qawery <wooki1@op.pl>
2016-08-06 16:07:02 +0200

    Minion Control

--- slavic/Assets/Scripts/AgentAI/MinionControll.cs
+++ slavic/Assets/Scripts/AgentAI/MinionControll.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class MinionControll : MonoBehaviour 
 {
+    private PatrolAI patrolAI;
+    private EquipmentManager equipmentManager;
 
 	void Start () 
     {
-	
+        patrolAI = GetComponent<PatrolAI>();
+        equipmentManager = GetComponent<EquipmentManager>();
 	}
 }
