8730cdb350ab06891776eeaba7c59af16281739b
Qawery <wooki1@op.pl>
2016-08-06 09:43:06 +0200

    Movement

--- slavic/Assets/Scripts/PlayerMovement.cs
+++ slavic/Assets/Scripts/PlayerMovement.cs
@@ -1,58 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour 
+{
+    private GameObject controlledMinion;
     private Vector3 currentCameraPositionOffset = new Vector3(0.0f, 0.0f, 0.0f);
     private Vector2 movementDirection;
     public Transform currentCamera;
     public float speed =1f;
     public Rigidbody2D playerRig;
-    public Equipment weapon_1;
 
 	// Use this for initialization
-	void Start () {
-        if (currentCamera != null)
-        {
-            currentCameraPositionOffset = currentCamera.transform.position - playerRig.transform.position;
-        }
+	void Start () 
+    {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        Move();
-        AimAndShoot();
-        if (currentCamera != null)
+	void Update () 
+    {
+        if (controlledMinion != null)
         {
-            CameraMovement();
+            Move();
+            AimAndShoot();
+            if (currentCamera != null)
+            {
+                CameraMovement();
+            }
         }
-	
 	}
        
-    void Move() {
-        if (currentCamera != null){
-
-            movementDirection.y = Input.GetAxisRaw("Vertical");
-            movementDirection.x = Input.GetAxisRaw("Horizontal");
-            movementDirection = movementDirection.normalized ;
-            if (playerRig != null)
-            {
-                playerRig.velocity = movementDirection * speed *Time.deltaTime;
-            }
+    void Move() 
+    {
+        movementDirection.y = Input.GetAxisRaw("Vertical");
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection = movementDirection.normalized;
+        if (playerRig != null)
+        {
+            playerRig.velocity = movementDirection * speed * Time.deltaTime;
         }
     }
     void AimAndShoot()
     {
         Vector3 observationPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-        //observationPoint.y = 0.5f;
-        Debug.Log(observationPoint);
+        
         //Strzał
         if (Input.GetButtonDown("Fire1"))
         {
-            if (weapon_1 != null)
-            {
-                weapon_1.Aim(observationPoint);
-                weapon_1.Fire();
-            }
+            //TODO: strzelanie
         }
     }
     private void CameraMovement()
