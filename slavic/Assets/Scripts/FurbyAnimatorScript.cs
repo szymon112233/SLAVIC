37e49c317ac14e4dffce6a5b645edb63d8558b3b
Schwalbe1728@gmail.com <schwalbe1728@gmail.com>
2016-08-06 17:03:41 +0200

    Corrected animations, working and scripted state machine.

---
+++ slavic/Assets/Scripts/FurbyAnimatorScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurbyAnimatorScript : MonoBehaviour {
+
+    private Animator animator;
+
+	void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }        
+
+	public void PlayDeathAnimation()
+    {
+        animator.SetTrigger("Die");
+    }
+
+    public void PlayJumpAnimation()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
