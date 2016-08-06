2311a26c32c8c93d9a5a97fd70c09821b032f41f
szymon112233 <szymmon123@gmail.com>
2016-08-06 11:47:28 +0200

    Added object finding

--- slavic/Assets/Scripts/GameplayManager.cs
+++ slavic/Assets/Scripts/GameplayManager.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class GameplayManager : MonoBehaviour {
 
+    //References to all Mechanics
+    public GameObject teamManager, mainCamera, squadManger, playerControll;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        teamManager = GameObject.Find("TeamManager");
+        mainCamera = GameObject.Find("Main Camera");
+        squadManger = GameObject.Find("SquadManager");
+        playerControll = GameObject.Find("PlayerControll");
+    }
 	
 	// Update is called once per frame
 	void Update () {
