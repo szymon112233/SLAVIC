6484ddba02f0ebd7a0e0b3b35c88a4639e6e6bc4
Qawery <wooki1@op.pl>
2016-08-07 08:01:26 +0200

    pauza

--- slavic/Assets/Scripts/GameplayManager.cs
+++ slavic/Assets/Scripts/GameplayManager.cs
@@ -10,6 +10,7 @@ public class GameplayManager : MonoBehaviour
     public SquadManager squadManager;
     public MinionControll playerControlledMinion;
     public Text annoucmentText;
+    private string previousAnnoucment;
     private bool isEndLevel;
     private float endLevelTimeout = 3f;
     private bool isPause;
@@ -31,21 +32,24 @@ public class GameplayManager : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (isPause)
             {
                 isPause = false;
+                annoucmentText.text = previousAnnoucment;
                 Time.timeScale = 1;
             }
             else
             {
                 isPause = true;
+                previousAnnoucment = annoucmentText.text;
+                annoucmentText.text = "Pause";
                 Time.timeScale = 0;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.LoadLevel("MainMenu");
         }
