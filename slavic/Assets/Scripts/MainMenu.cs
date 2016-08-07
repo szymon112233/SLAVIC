b411e18a9d8d7427496fa3467b4e52cc9b5fa32f
Jacek <jacek.kozieja@gmail.com>
2016-08-07 04:18:19 +0200

    Main Menu

---
+++ slavic/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        Application.LoadLevel(Application.loadedLevel+1);
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+}
