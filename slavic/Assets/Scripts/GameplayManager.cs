4d968cb201a68fd082a81b1723d442317c87ca09
Qawery <wooki1@op.pl>
2016-08-07 05:48:59 +0200

    l

--- slavic/Assets/Scripts/GameplayManager.cs
+++ slavic/Assets/Scripts/GameplayManager.cs
@@ -8,12 +8,20 @@ public class GameplayManager : MonoBehaviour
     public EffectsManager effectsManager;
     public SquadManager squadManager;
     public MinionControll playerControlledMinion;
-	
+
+    private bool isDefeat;
+
     void Start () 
     {
+        isDefeat = false;
     }
 	
 	void Update () 
     {
 	}
+
+    public void Defeat()
+    {
+        isDefeat = true;
+    }
 }
